f474d4403d69c96b951fe2d7ebcb572f4eaa4c64
khj <khajit.sorcerer@gmail.com>
2017-12-01 15:55:15 +0200

    Added 2 more windows, but not finished them yet.

--- ChatAppWpf/ClientWindow.xaml.cs
+++ ChatAppWpf/ClientWindow.xaml.cs
@@ -18,9 +18,9 @@ namespace ChatAppWpf
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ClientWindow : Window
     {
-        public MainWindow()
+        public ClientWindow()
         {
             InitializeComponent();
 
---
+++ ChatAppWpf/StartupWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChatAppWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для StartupWindow.xaml
+    /// </summary>
+    public partial class StartupWindow : Window
+    {
+        public StartupWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonServer_Click(object sender, RoutedEventArgs e)
+        {
+            ServerWindow win = new ServerWindow();
+            win.Left = this.Left;
+            win.Top = this.Top;
+            win.Show();
+            this.Close();
+        }
+
+        private void ButtonClient_Click(object sender, RoutedEventArgs e)
+        {
+            ClientWindow win = new ClientWindow();
+            win.Left = this.Left;
+            win.Top = this.Top;
+            win.Show();
+            this.Close();
+        }
+    }
+}
